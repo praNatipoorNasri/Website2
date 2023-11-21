b038b419e68d04ece742c56cbf2d02e40a456434
praNatipoorNasri <151111044+praNatipoorNasri@users.noreply.github.com>
2023-11-21 11:17:33 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,18 @@
+﻿function handleSubmit () {
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('surname').value;
+    const score = document.getElementById('score').value;
+
+    // to set into local storage
+    /* localStorage.setItem("NAME", name);
+    localStorage.setItem("SURNAME", surname);
+     localStorage.setItem("SCORE", score)*/
+    
+    sessionStorage.setItem("NAME", name);
+    sessionStorage.setItem("SURNAME", surname);
+    sessionStorage.setItem("SCORE", score);
+
+
+    return;
+}
+
---
+++ result.js
@@ -0,0 +1,24 @@
+﻿window.addEventListener('load', () => {
+
+    // Via Query parameters - GET
+    /* const params = (new URL(document.location)).searchParams;
+    const name = params.get('name');
+    const surname = params.get('surname'); 
+    const score = params.get('score'); */
+
+    // Via local Storage
+    /* const name = localStorage.getItem('NAME');
+    const surname = localStorage.getItem('SURNAME'); 
+    const score = localStorage.getItem('score') */
+    
+    const name = sessionStorage.getItem('NAME');
+    const surname = sessionStorage.getItem('SURNAME');
+    const score = sessionStorage.getItem('SCORE');
+    
+    document.getElementById('result-name').innerHTML = name;
+    document.getElementById('result-surname').innerHTML = surname;
+    document.getElementById('result-score').innerHTML = score;
+
+
+})
+
